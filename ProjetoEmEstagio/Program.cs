29de28f70497cd37695f2fc7b0a6ed759ed979db
Arthur Bomfim <arthurbao10@gmail.com>
2024-12-03 11:56:33 -0300

    func ale

--- ProjetoEmEstagio/Program.cs
+++ ProjetoEmEstagio/Program.cs
@@ -18,3 +18,24 @@ static void CriacaoMatriz<T>(T variavel)
         }
     }
 }
+
+static string[,] ValorDiferente(string[,] jogo)
+{
+    Random rnd = new Random();
+    int linha = rnd.Next(10);
+    int coluna = rnd.Next(24);
+    jogo[linha, coluna] = "0";
+    return jogo;
+}
+
+static void ExibirJogo(string[,] jogo)
+{
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 24; j++)
+        {
+            Console.Write(jogo[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
