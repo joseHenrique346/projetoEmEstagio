123e3eca41fdf2e40e96d7994fe5c22f65e586a4
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-03 11:49:10 -0300

    Initial commit

---
+++ ProjetoEmEstagio/Program.cs
@@ -0,0 +1,21 @@
+﻿Vermei("Menino Fatal");
+CriacaoMatriz("O");
+
+static void Vermei(string txt)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine(txt);
+    Console.ResetColor();
+}
+
+static void CriacaoMatriz<T>(T variavel)
+{
+    T[,] jogo = new T[11, 24];
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 24; j++)
+        {
+            jogo[i, j] = variavel;
+        }
+    }
+}
